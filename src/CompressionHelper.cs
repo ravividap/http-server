cfef6f794f6a9d6ed07c9f7a784ef1fd61ac81e5
Ravi Vidap <ravividap@ymail.com>
2025-04-12 12:48:00 +0530

    send compressed repoonse

---
+++ src/CompressionHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace codecrafters_http_server.src;
+
+public static class CompressionHelper
+{
+    public static byte[] CompressWithGzip(string data)
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress))
+            {
+                using (var writer = new StreamWriter(gzipStream, Encoding.UTF8))
+                {
+                    writer.Write(data);
+                }
+            }
+
+            return memoryStream.ToArray();
+        }
+    }
+
+    public static bool ClientSupportsGzip(HttpRequest request)
+    {
+        if (request.Headers.TryGetValue("Accept-Encoding", out string encodings))
+        {
+            return encodings.Split(',')
+                .Select(e => e.Trim().ToLowerInvariant())
+                .Contains("gzip");
+        }
+        return false;
+    }
+}
